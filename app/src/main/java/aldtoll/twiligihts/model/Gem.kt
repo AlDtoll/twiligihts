a388cce04792ae0f5c76d16e4eb852d91500eeee
AlDToll <aledtoll@gmail.com>
2024-01-16 15:28:10 +0700

    add more colors

--- app/src/main/java/aldtoll/twiligihts/model/Gem.kt
+++ app/src/main/java/aldtoll/twiligihts/model/Gem.kt
@@ -24,6 +24,10 @@ data class Gem(
                 4 -> R.color.gem_color_4
                 5 -> R.color.gem_color_5
                 6 -> R.color.gem_color_6
+                7 -> R.color.gem_color_7
+                8 -> R.color.gem_color_8
+                9 -> R.color.gem_color_9
+                10 -> R.color.gem_color_10
                 else -> R.color.default_color
             }
         }
