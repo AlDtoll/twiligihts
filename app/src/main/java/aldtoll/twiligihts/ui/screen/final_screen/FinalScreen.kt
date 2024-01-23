3cee477643945be508811ba175ccae11f449741e
AlDToll <aledtoll@gmail.com>
2024-01-23 17:26:44 +0700

    add final screen

---
+++ app/src/main/java/aldtoll/twiligihts/ui/screen/final_screen/FinalScreen.kt
@@ -0,0 +1,34 @@
+package aldtoll.twiligihts.ui.screen.final_screen
+
+import aldtoll.twiligihts.R
+import aldtoll.twiligihts.databinding.FragmentFinalScreenBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FinalScreen : Fragment() {
+
+    private lateinit var binding: FragmentFinalScreenBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFinalScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            findNavController().navigate(R.id.action_finalScreen_to_startScreenFragment)
+        }
+    }
+}
--- app/src/main/java/aldtoll/twiligihts/ui/screen/game_screen/GameScreen.kt
+++ app/src/main/java/aldtoll/twiligihts/ui/screen/game_screen/GameScreen.kt
@@ -1,5 +1,6 @@
 package aldtoll.twiligihts.ui.screen.game_screen
 
+import aldtoll.twiligihts.R
 import aldtoll.twiligihts.databinding.FragmentGameScreenBinding
 import aldtoll.twiligihts.ext.addChangeAnimation
 import aldtoll.twiligihts.ext.checkPossibleMoves
@@ -18,6 +19,7 @@ import android.view.animation.AccelerateInterpolator
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
@@ -281,6 +283,9 @@ class GameScreen : Fragment() {
             val wound = "${it.wounds}/${it.maxWounds} Ран"
             binding.personWounds.text = wound
             statusAdapter.updateData(ArrayList(it.statuses.map { status -> status.copy() }))
+            if (it.hp == 0) {
+                findNavController().navigate(R.id.finalScreen)
+            }
         }
         binding.personStatus.setOnClickListener {
             if (binding.statusList.visibility == View.VISIBLE) {
