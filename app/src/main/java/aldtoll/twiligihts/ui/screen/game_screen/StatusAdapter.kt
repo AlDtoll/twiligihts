3919c556a710aac073aee266d93a312c18647b23
AlDToll <aledtoll@gmail.com>
2024-03-08 19:09:36 +0700

    add status icon

--- app/src/main/java/aldtoll/twiligihts/ui/screen/game_screen/StatusAdapter.kt
+++ app/src/main/java/aldtoll/twiligihts/ui/screen/game_screen/StatusAdapter.kt
@@ -1,10 +1,14 @@
 package aldtoll.twiligihts.ui.screen.game_screen
 
+import aldtoll.twiligihts.R
 import aldtoll.twiligihts.databinding.ItemStatusBinding
 import aldtoll.twiligihts.model.Status
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -64,8 +68,13 @@ class StatusAdapter : RecyclerView.Adapter<StatusAdapter.StatusHolder>() {
             } else {
                 binding.statusBlock.visibility = View.GONE
             }
-            binding.statusName.text = status.name
-            binding.statusValue.text = status.value.toString()
+            binding.statusBlock.setBackgroundColor(
+                ContextCompat.getColor(
+                    binding.root.context,
+                    getStatusColor(status.type)
+                )
+            )
+            binding.statusName.text = "${status.name}: ${status.value}"
             val duration = if (status.isInfinity()) {
                 "вечно"
             } else {
@@ -79,6 +88,47 @@ class StatusAdapter : RecyclerView.Adapter<StatusAdapter.StatusHolder>() {
             } else {
                 binding.statusDescription.visibility = View.GONE
             }
+            binding.statusIcon.setImageResource(getStatusIcon(status.type))
+        }
+    }
+
+    @DrawableRes
+    private fun getStatusIcon(type: Status.EffectType): Int {
+        return when (type) {
+            Status.EffectType.DODGE -> R.drawable.ic_dodge
+            Status.EffectType.SMART_DODGE -> R.drawable.ic_dodge
+            Status.EffectType.GAIN -> R.drawable.ic_gain
+            Status.EffectType.REDUCE -> R.drawable.ic_reduce
+            Status.EffectType.WEAK -> R.drawable.ic_weak
+            Status.EffectType.STRONG -> R.drawable.ic_strong
+            Status.EffectType.VULNERABLE -> R.drawable.ic_vul
+            Status.EffectType.ARMOR -> R.drawable.ic_armor
+            Status.EffectType.COUNTERATTACK -> R.drawable.ic_counterattack
+            Status.EffectType.HARM -> R.drawable.ic_spikes
+            Status.EffectType.INFO -> R.drawable.ic_info
+            Status.EffectType.DAMAGE -> R.drawable.ic_damage
+            Status.EffectType.HEAL -> R.drawable.ic_heal
+            Status.EffectType.GENERATE -> R.drawable.ic_generate
+        }
+    }
+
+    @ColorRes
+    private fun getStatusColor(type: Status.EffectType): Int {
+        return when (type) {
+            Status.EffectType.DODGE -> R.color.light_green_background_color
+            Status.EffectType.SMART_DODGE -> R.color.light_green_background_color
+            Status.EffectType.GAIN -> R.color.light_green_background_color
+            Status.EffectType.REDUCE -> R.color.light_red_background_color
+            Status.EffectType.WEAK -> R.color.light_red_background_color
+            Status.EffectType.STRONG -> R.color.light_green_background_color
+            Status.EffectType.VULNERABLE -> R.color.light_red_background_color
+            Status.EffectType.ARMOR -> R.color.light_green_background_color
+            Status.EffectType.COUNTERATTACK -> R.color.light_green_background_color
+            Status.EffectType.HARM -> R.color.light_green_background_color
+            Status.EffectType.INFO -> R.color.light_blue_background_color
+            Status.EffectType.DAMAGE -> R.color.light_red_background_color
+            Status.EffectType.HEAL -> R.color.light_green_background_color
+            Status.EffectType.GENERATE -> R.color.light_blue_background_color
         }
     }
 }
